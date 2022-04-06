1552f50151bfd026476ee05547bf4f38ab14f8b6
mecaneer23 <mecaneer23@gmail.com>
2022-04-06 12:24:44 -0500

    change functionality

--- pymatrix.py
+++ pymatrix.py
@@ -7,10 +7,10 @@ from string import ascii_letters, digits
 
 def main(stdscr):
     CHARS = [*ascii_letters, *digits]
-    ROWS = stdscr.getmaxyx()[0] - 1
-    COLS = stdscr.getmaxyx()[1] - 1
+    ROWS = stdscr.getmaxyx()[0]
+    COLS = stdscr.getmaxyx()[1]
 
-    col_array = [0 for _ in range(COLS)]
+    col_array = [[0] for _ in range(COLS)]
 
     curses.curs_set(0)
     curses.use_default_colors()
@@ -24,11 +24,20 @@ def main(stdscr):
             return
         if char in (113, 27):  # q or esc
             return
-        for col in range(COLS):
-            stdscr.addch(col_array[col], col, choice(CHARS))
-            if col_array[col] == ROWS:
-                col_array[col] = 0
-            col_array[col] += choice((0, 1, 1))
+        for col in col_array:
+            if choice((0, 1, 1)):
+                col.append(choice(CHARS))
+            if len(col) >= ROWS:
+                col = []
+                break
+            else:
+                for i, char in enumerate(col):
+                    stdscr.addstr(len(col) - i, col_array.index(col), str(char))
+                    stdscr.refresh()
+        #     stdscr.addch(col_array[col], col, choice(CHARS))
+        #     if col_array[col] == ROWS:
+        #         col_array[col] = 0
+        #     col_array[col] += choice((0, 1, 1))
 
 if __name__ == "__main__":
     return_code = curses.wrapper(main)
